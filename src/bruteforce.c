c04c55cf04c378b2efc45b927ac09fe0d92120f7
Elias Andersen <eoan24@student.aau.dk>
2024-11-06 14:13:27 +0100

    current state of brute force

--- src/bruteforce.c
+++ src/bruteforce.c
@@ -22,28 +22,59 @@ double calculateLength(Mines mine1, Mines mine2) {
 }
 
 
-Mines *allCombinations(Mines *mines, unsigned int arrSize) {
-    Mines *list1 = (Mines *)malloc(sizeof(mines[0])*arrSize*factorial(arrSize));
-
-    if (list1 == NULL)
-        exit(EXIT_FAILURE);
-
-    unsigned int currentList = 0;
-
-    for (unsigned int i = 0; i < arrSize; i++) {
-        for (unsigned int j = 0; j < arrSize; j++) {
-            if (j == i) continue;
-            for (unsigned int k = 0; k < arrSize; k++) {
-                if (k == j || k == i) continue;
-                list1[currentList++] = mines[i];
-                list1[currentList++] = mines[j];
-                list1[currentList++] = mines[k];
-            }
-        }
-    }
-    return list1;
+Mines **allCombinations(Mines *mines, unsigned int arraySize) {
+    // array over arrays of paths. Size of the arraypointer times the amount of arrays
+    Mines **listOfPaths = (Mines **)malloc(factorial(arraySize) * sizeof(Mines *));
+    // check if malloc was a succes
+    if (listOfPaths == NULL) exit(EXIT_FAILURE);
+    // current path which is being found for in listOfMineFields
+    // maybe no reason to have this. since its mines
+    Mines *currentPath = (Mines *)malloc(arraySize * sizeof(Mines));
+    if (currentPath == NULL) exit(EXIT_FAILURE);
+    for (unsigned int i = 0; i < arraySize; i++)
+        currentPath[i] = mines[i];
+    // currentPath will be a var for keeping track of which mines that have already been meetj 
+    unsigned int idx = 0;
+    findPaths(listOfPaths, mines, currentPath, 0, arraySize, &idx);
+    // the current path isnt needed any more only the list of mine fields
+    //free(currentPath);
+    return listOfPaths;
 }   
 
+void findPaths(Mines **listOfPaths, Mines *mines, Mines *path, unsigned int depth, unsigned int arraySize, unsigned int *currentArray) {
+    if (depth == arraySize) {
+        // make space for this path in the list of all paths
+        listOfPaths[*currentArray] = (Mines *)malloc(arraySize * sizeof(Mines)); 
+        if (listOfPaths[*currentArray] == NULL) { // check if allocation was succesfull
+            fprintf(stderr, "list of paths allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        // saved the found path
+        for (unsigned int i = 0; i < arraySize; i++) 
+            listOfPaths[*currentArray][i] = path[i];
+        // update the amount of paths found
+        (*currentArray)++;
+        // path found so dont do anything in this path 
+        return;
+    }
+    
+    for (unsigned int i = 0; i < arraySize - depth; i++) {
+        // allocate space for remaining mines of each path
+        path[depth] = mines[i];
+        Mines *remainingMines = (Mines *)malloc(sizeof(Mines) * arraySize - depth - 1);
+        if (remainingMines == NULL) {
+            fprintf(stderr, "remainingMines allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        // copy mines over in remaining mines but not the one already in the path
+        for (unsigned int j = 0; j > arraySize - depth - 1; j++) { 
+            if (j == i) continue;
+            remainingMines[j] = mines[j];
+        }
+        findPaths(listOfPaths, remainingMines, path, depth+1, arraySize - depth - 1, currentArray + 1); 
+        free(remainingMines);
+    }
+}
 
 unsigned int factorial(unsigned int num) {
     unsigned int fact = 1;
