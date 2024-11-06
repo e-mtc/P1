d955743de395ff37e55e48c7130f0cf561b02cfe
Sebastian <sgje24@student.aau.dk>
2024-11-06 15:10:52 +0100

    Indented in header file to make visible which function is used by which

--- src/LowBoundTSP.h
+++ src/LowBoundTSP.h
@@ -1,8 +1,8 @@
 #define VALUES 3
 #define WEIGHT 2
-void BubbleSort(int paths, int a[paths][VALUES]);
-void kruskalAlgo(int paths, int sortedArray[paths][VALUES]);
-void makeSet(int parent[], int rank[], int n);
-int findParent(int parent[], int component);
-void unionSet(int u, int v, int parent[], int rank[]);
-void makeMST(int paths, int travelArray[paths][VALUES]);
+void makeMST(int paths, int travelArray[paths][VALUES]); //------------------
+    void BubbleSort(int paths, int a[paths][VALUES]);
+    void kruskalAlgo(int paths, int sortedArray[paths][VALUES]); //-----------------
+        void makeSet(int parent[], int rank[], int n);
+        int findParent(int parent[], int component);
+        void unionSet(int u, int v, int parent[], int rank[]);
