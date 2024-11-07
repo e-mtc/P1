95ef43e32c0f37a5e1656ad7889e3c89a7ee04b8
Elias Andersen <eoan24@student.aau.dk>
2024-11-07 13:25:46 +0100

    added function descriptions in header

--- src/bruteforce.c
+++ src/bruteforce.c
@@ -3,93 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-double calculateLength(Mines mine1, Mines mine2) {
-    Mines temp;
-    
-    if (mine1.x < mine2.x) {
-        temp.x = mine1.x;
-        mine1.x = mine2.x;
-        mine2.x = temp.x;
-    }
 
-    if (mine1.y < mine2.y) {
-        temp.y = mine1.y;
-        mine1.y = mine2.y;
-        mine2.y = temp.y;
-    }
 
-    return sqrt(pow(mine1.x - mine2.x, 2) + pow(mine1.y - mine2.y, 2));
-}
 
-Mines **getPaths(const Mines *mines, unsigned int arraySize) {
-    // array over arrays of paths. Size of the arraypointer times the amount of arrays
-    Mines **listOfPaths = (Mines **)malloc(factorial(arraySize) * sizeof(Mines *));
-    // check if malloc was a succes
-    if (listOfPaths == NULL) exit(EXIT_FAILURE);
-    // currentPath will be a var for keeping track of which mines that have already been met 
-    Mines *currentPath = (Mines *)malloc(arraySize * sizeof(Mines));
-    // check if malloc was a succes
-    if (currentPath == NULL) exit(EXIT_FAILURE);
-    // index that keeås track of which combination is being made
-    unsigned int idx = 0;
-    // find all the diffeneret paths
-    findPaths(listOfPaths, mines, currentPath, 0, arraySize, &idx);
-    // the current path isnt needed any more only the list of mine fields
-    free(currentPath);
-    return listOfPaths;
-}   
-
-// TODO make it so every path leads back to the starting one
-void findPaths(Mines **listOfPaths, const Mines *mines, Mines *path, unsigned int depth, const unsigned int arraySize, unsigned int *currentArray) {
-    // depth starts at zero so arraySize - 1
-    if (depth == arraySize - 1) {
-        // make space for this path in the list of all paths
-        listOfPaths[*currentArray] = (Mines *)malloc(arraySize * sizeof(Mines)); 
-        // check if allocation was succesfull
-        if (listOfPaths[*currentArray] == NULL) {
-            fprintf(stderr, "list of paths allocation failed");
-            exit(EXIT_FAILURE);
-        }
-        // set last step to the remaining mine 
-        path[depth] = mines[0];
-        // saved the found path
-        for (unsigned int i = 0; i < arraySize; i++)  
-            listOfPaths[*currentArray][i] = path[i];
-        // update the amount of paths found
-        (*currentArray)++;
-    }
-    // make every mine a possible path
-    for (unsigned int i = 0; i < arraySize - depth; i++) {
-        // make the next step mine i
-        path[depth] = mines[i];
-        // allocate space for remaining mines of each path
-        Mines *remainingMines = (Mines *)malloc(sizeof(Mines) * (arraySize - depth - 1));
-        // check if malloc was a succes
-        if (remainingMines == NULL) {
-            fprintf(stderr, "remainingMines allocation failed");
-            exit(EXIT_FAILURE);
-        }
-        // copy mines over in remaining mines but not the one already in the path
-        int mineNr = 0;
-        for (unsigned int j = 0; j < arraySize - depth; j++) { 
-            if (j == i) continue;
-            remainingMines[mineNr++] = mines[j];
-        }
-        // recursively call the function with the remaining mines and one step deeper
-        findPaths(listOfPaths, remainingMines, path, depth + 1, arraySize, currentArray); 
-        // free remainingMines as they aren't used anymore
-        free(remainingMines);
-    }
-}
-
-double pathLength(const Mines *path, unsigned int arraySize) {
-    // variable that stores the length of the path
-    double length = 0;
-    // calculate the length of the entire path
-    for (unsigned int idx = 1; idx < arraySize; idx++) 
-        length += calculateLength(path[idx - 1], path[idx]); 
-    return length;
-}
 
 Mines *getShortestPath(const Mines *mines, unsigned int arraySize) {
     // gets every combination 
@@ -130,11 +46,92 @@ Mines *shortestPath(Mines **minefields, unsigned int minefieldscount, unsigned i
     return shortestRoute;    
 }
 
+Mines **getPaths(const Mines *mines, unsigned int arraySize) {
+    // array over arrays of paths. Size of the arraypointer times the amount of arrays
+    Mines **listOfPaths = (Mines **)malloc(factorial(arraySize) * sizeof(Mines *));
+    if (listOfPaths == NULL) exit(EXIT_FAILURE);
+    // currentPath will be a var for keeping track of which mines that have already been met 
+    Mines *currentPath = (Mines *)malloc(arraySize * sizeof(Mines));
+    if (currentPath == NULL) exit(EXIT_FAILURE);
+    // index that keeås track of how many paths have been found
+    unsigned int idx = 0;
+    // find all the diffeneret paths
+    findPaths(listOfPaths, mines, currentPath, 0, arraySize, &idx);
 
-unsigned int factorial(unsigned int num) {
-    unsigned int fact = 1;
-    for (unsigned int i = 1; i <= num; i++) {
-        fact *= i;
+    free(currentPath);
+    return listOfPaths;
+}   
+
+// TODO make it so every path leads back to the starting one
+void findPaths(Mines **listOfPaths, const Mines *mines, Mines *path, unsigned int depth, const unsigned int arraySize, unsigned int *currentArray) {
+    // depth starts at zero so arraySize - 1
+    if (depth == arraySize - 1) {
+        // make space for this path in the list of all paths
+        listOfPaths[*currentArray] = (Mines *)malloc(arraySize * sizeof(Mines)); 
+        if (listOfPaths[*currentArray] == NULL) {
+            fprintf(stderr, "list of paths allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        // set last step to the remaining mine 
+        path[depth] = mines[0];
+        // saved the found path
+        for (unsigned int i = 0; i < arraySize; i++)  
+            listOfPaths[*currentArray][i] = path[i];
+        // update the amount of paths found
+        (*currentArray)++;
     }
-    return fact;
+    // make every mine a possible path
+    for (unsigned int i = 0; i < arraySize - depth; i++) {
+        // make the next step mine i
+        path[depth] = mines[i];
+        // allocate space for remaining mines of each path
+        Mines *remainingMines = (Mines *)malloc(sizeof(Mines) * (arraySize - depth - 1));
+        if (remainingMines == NULL) {
+            fprintf(stderr, "remainingMines allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        // copy mines over in remaining mines but not the one already in the path
+        int mineNr = 0;
+        for (unsigned int j = 0; j < arraySize - depth; j++) { 
+            if (j == i) continue;
+            remainingMines[mineNr++] = mines[j];
+        }
+        // recursively call the function with the remaining mines and one step deeper
+        findPaths(listOfPaths, remainingMines, path, depth + 1, arraySize, currentArray); 
+        free(remainingMines);
+    }
+}
+
+double pathLength(const Mines *path, unsigned int arraySize) {
+    // variable that stores the length of the path
+    double length = 0;
+    // calculate the length of the entire path
+    for (unsigned int idx = 1; idx < arraySize; idx++) 
+        length += calculateLength(path[idx - 1], path[idx]); 
+    return length;
+}
+
+double calculateLength(Mines mine1, Mines mine2) {
+    Mines temp; // temp variable for swapping mine values
+    // swapvalues to avoid negative numbers 
+    if (mine1.x < mine2.x) {
+        temp.x = mine1.x;
+        mine1.x = mine2.x;
+        mine2.x = temp.x;
+    }
+    if (mine1.y < mine2.y) {
+        temp.y = mine1.y;
+        mine1.y = mine2.y;
+        mine2.y = temp.y;
+    }
+    // calculate distance between two mines and return
+    return sqrt(pow(mine1.x - mine2.x, 2) + pow(mine1.y - mine2.y, 2));
+}
+
+unsigned int factorial(unsigned int number) {
+    // variable for keeping track of product 
+    unsigned int product = 1;
+    for (unsigned int n = 1; n <= number; n++) 
+        product *= n; // find the product of every number from 1 to number (input) 
+    return product;
 }
--- src/bruteforce.h
+++ src/bruteforce.h
@@ -4,20 +4,54 @@ typedef struct {
     unsigned int y;
 } Mines;
 
-// the usefull function
-// @param mines the list of the mines in the minefield
-// @param arraySize the number of mines in the minefield
-// @return returns an array of the shortest path through the minefield
+/* 
+ * @param mines the list of the mines in the minefield
+ * @param arraySize the number of mines in the minefield
+ * @return returns an array of the shortest path through the minefield
+ */
 Mines *getShortestPath(const Mines *mines, unsigned int arraySize);
 
-Mines **getPaths(const Mines *mines, unsigned int arrSize);
-
-double calculateLength(Mines mine1, Mines mine2);
-
-void findPaths(Mines **listOfPaths, const Mines *mines, Mines *path, unsigned int depth, const unsigned int arraySize, unsigned int *currentArray);
-
+/*
+ * @param
+ * @param
+ * @param
+ * @return
+ */
 Mines *shortestPath(Mines **minefields, unsigned int minefieldscount, unsigned int arraySize);
 
+/*
+ * @param mines       the mines in a minefield 
+ * @param arraySize   the amount of mines in a minefield
+ * @return a list of every path (2D array) 
+ */
+Mines **getPaths(const Mines *mines, unsigned int arraySize);
+
+/*
+ * @param listOfPaths     2D list of possible paths
+ * @param mines           the mines in the minefield
+ * @param path            keeps track of the mines in current path (it changes)
+ * @param depth           how deep in the path the function is
+ * @param arraySize       amount of mines in minefield
+ * @param currentArray    keeps track of how many paths have been found
+ * @return void - changes values of listOfPaths
+ */
+void findPaths(Mines **listOfPaths, const Mines *mines, Mines *path, unsigned int depth, const unsigned int arraySize, unsigned int *currentArray);
+
+/*
+ * @param path       array of mines in path
+ * @param arraySize  amount of mines in path
+ * @return length of path (double)
+ */
 double pathLength(const Mines *path, unsigned int arraySize);
 
-unsigned int factorial(unsigned int num); 
+/*
+ * @param mine1 & mine2   the mines to find the distance between 
+ * @return the distance between two mines
+ */
+double calculateLength(Mines mine1, Mines mine2);
+
+/*
+ * @param number  the number to do the factorial calculation on
+ * @return        factorial of the input number
+ */
+unsigned int factorial(unsigned int number); 
