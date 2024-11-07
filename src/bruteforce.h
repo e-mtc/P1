754986f35a3ca694e8d742d339a2fe492336139f
Elias Andersen <eoan24@student.aau.dk>
2024-11-07 12:26:44 +0100

    added description to the usefull function (getShortestPath)

--- src/bruteforce.h
+++ src/bruteforce.h
@@ -4,16 +4,20 @@ typedef struct {
     unsigned int y;
 } Mines;
 
-double calculateLength(Mines mine1, Mines mine2);
+// the usefull function
+// @param mines the list of the mines in the minefield
+// @param arraySize the number of mines in the minefield
+// @return returns an array of the shortest path through the minefield
+Mines *getShortestPath(const Mines *mines, unsigned int arraySize);
 
 Mines **getPaths(const Mines *mines, unsigned int arrSize);
+
+double calculateLength(Mines mine1, Mines mine2);
 
 void findPaths(Mines **listOfPaths, const Mines *mines, Mines *path, unsigned int depth, const unsigned int arraySize, unsigned int *currentArray);
 
 Mines *shortestPath(Mines **minefields, unsigned int minefieldscount, unsigned int arraySize);
 
 double pathLength(const Mines *path, unsigned int arraySize);
-
-Mines *getShortestPath(const Mines *mines, unsigned int arraySize);
 
 unsigned int factorial(unsigned int num); 
