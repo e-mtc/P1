c6bb4e935d5ddcd46ed493b4fcc7b4bcee71fdc2
Elias Andersen <eoan24@student.aau.dk>
2024-11-07 20:06:36 +0100

    tried writing all combination function to work with whatever array of elements (could maybe just have used integers as index)

--- src/bruteforce.c
+++ src/bruteforce.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <string.h>
+
+/*
 Mines *getShortestPath(const Mines *mines, unsigned int arraySize) {
     // gets every combination 
     Mines **paths = getPaths(mines, arraySize);
@@ -41,62 +44,54 @@ Mines *shortestPath(Mines **minefields, unsigned int minefieldscount, unsigned i
          shortestRoute[idx] = minefields[shrtpth][idx];
     return shortestRoute;    
 }
+*/
 
-// TODO make it so every path leads back to the starting one
-Mines **getPaths(const Mines *mines, unsigned int arraySize) {
-    // array over arrays of paths. Size of the arraypointer times the amount of arrays
-    Mines **listOfPaths = (Mines **)malloc(factorial(arraySize) * sizeof(Mines *));
-    if (listOfPaths == NULL) exit(EXIT_FAILURE);
-    // currentPath will be a var for keeping track of which mines that have already been met 
-    Mines *currentPath = (Mines *)malloc(arraySize * sizeof(Mines));
-    if (currentPath == NULL) exit(EXIT_FAILURE);
-    // index that keeås track of how many paths have been found
-    unsigned int idx = 0;
-    // find all the diffeneret paths
-    findPaths(listOfPaths, mines, currentPath, 0, arraySize, &idx);
+void **getPaths(void *elements, unsigned int elementSize, unsigned int arraySize) {
+    unsigned int combinations = factorial(arraySize);
 
+    void **paths = (void **)malloc(sizeof(void *) * combinations);
+    if (paths == NULL) fprintf(stderr, "paths err");
+
+    void *currentPath = (void *)malloc(elementSize * arraySize);
+    if (currentPath == NULL) fprintf(stderr, "currentPath err");
+
+    int idx = 0;
+    findPaths(paths, elements, currentPath, elementSize, 0, arraySize, &idx);
+    
     free(currentPath);
-    return listOfPaths;
+    return paths;
 }   
 
-void findPaths(Mines **listOfPaths, const Mines *mines, Mines *path, unsigned int depth, const unsigned int arraySize, unsigned int *currentArray) {
-    // depth starts at zero so arraySize - 1
-    if (depth == arraySize - 1) {
-        // make space for this path in the list of all paths
-        listOfPaths[*currentArray] = (Mines *)malloc(arraySize * sizeof(Mines)); 
-        if (listOfPaths[*currentArray] == NULL) {
-            fprintf(stderr, "list of paths allocation failed");
-            exit(EXIT_FAILURE);
-        }
-        // set last step to the remaining mine 
-        path[depth] = mines[0];
-        // saved the found path
-        for (unsigned int i = 0; i < arraySize; i++)  
-            listOfPaths[*currentArray][i] = path[i];
-        // update the amount of paths found
+void findPaths(void **listOfPaths, const void *elements, void *path, unsigned int elementSize, unsigned int depth, const unsigned int arraySize, unsigned int *currentArray) {
+    if (depth == arraySize) {
+        listOfPaths[*currentArray] = (void *)malloc(arraySize * elementSize);
+        if (listOfPaths[*currentArray] == NULL) fprintf(stderr, "currentArray err");
+
+        memcpy(listOfPaths[*currentArray], path, arraySize * elementSize);
+
         (*currentArray)++;
-    }
-    // make every mine a possible path
+    }            
+    
+    // difficulty copying only path of array
+    // Maybe try:
+    // use memcpy -> typecast to (char *) * elementSize = to move one element in array
     for (unsigned int i = 0; i < arraySize - depth; i++) {
-        // make the next step mine i
-        path[depth] = mines[i];
-        // allocate space for remaining mines of each path
-        Mines *remainingMines = (Mines *)malloc(sizeof(Mines) * (arraySize - depth - 1));
-        if (remainingMines == NULL) {
-            fprintf(stderr, "remainingMines allocation failed");
-            exit(EXIT_FAILURE);
-        }
-        // copy mines over in remaining mines but not the one already in the path
-        int mineNr = 0;
-        for (unsigned int j = 0; j < arraySize - depth; j++) { 
+        path[depth] = elements[i];
+
+        void *remainingElements = (void *)malloc(elementSize * (arraySize - depth - 1));
+        if (remainingElements == NULL) fprintf(stderr, "remainingElements err");
+
+        int elementNr = 0;
+        for (unsigned int j = 0; j < arraySize - depth; j++) {
             if (j == i) continue;
-            remainingMines[mineNr++] = mines[j];
+            remainingElements[elementNr++] = elements[j];
         }
-        // recursively call the function with the remaining mines and one step deeper
-        findPaths(listOfPaths, remainingMines, path, depth + 1, arraySize, currentArray); 
-        free(remainingMines);
+
+        findPaths(listOfPaths, remainingElements, path, elementSize, depth + 1, arraySize, currentArray);
+        free(remainingElements);
     }
 }
+
 
 double pathLength(const Mines *path, unsigned int arraySize) {
     // variable that stores the length of the path
