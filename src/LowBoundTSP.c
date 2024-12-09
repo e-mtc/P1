392cf35e8e2d53c59ae610fc16299e87c6eb37cd
SGronning <sgje24@student.aau.dk>
2024-12-09 18:36:35 +0100

    Tester med structs

--- src/LowBoundTSP.c
+++ src/LowBoundTSP.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <math.h>
 #include "LowBoundTSP.h"
 
@@ -9,6 +8,7 @@ double calculateDistance(mine_s bomb1, mine_s bomb2) {
     return (sqrt(pow(bomb1.x - bomb2.x, 2) + pow(bomb1.y - bomb2.y, 2))) * (bomb1.tw + bomb2.tw);
 }
 
+//Finds amount of edges in travelArray to be build
 int findEdgeAmount(int bombAmount) {
     int edgeAmount = 0;
     for (int i = bombAmount-1; 0 < i; i--) {
@@ -17,88 +17,77 @@ int findEdgeAmount(int bombAmount) {
     return bombAmount;
 }
 
-double **structTo2D(mine_s bombs[], int bombAmount, int edgeAmount) {
-    double **edgeArray = make2DArray(edgeAmount, 3);
-
+//Makes the struct array to a travel
+void structToStruct(mine_s bombs[], int bombAmount, int edgeAmount, edge_s edgeArray[edgeAmount]) {
     int pathsMade = 0;
+
     for (int i = 0; i < bombAmount; i++) {
         for (int j = 1+i; j < bombAmount; j++) {
-            edgeArray[pathsMade][0] = i;
-            edgeArray[pathsMade][1] = j;
-            edgeArray[pathsMade][2] = calculateDistance(bombs[i], bombs[j]);
+            edgeArray[pathsMade].sourceBomb = bombs[i];
+            edgeArray[pathsMade].destinationBomb = bombs[j];
+            edgeArray[pathsMade].distanceBetween = calculateDistance(bombs[i], bombs[j]);
             pathsMade++;
         }
     }
-
-    return edgeArray;
 }
 
 //Sorts the array
-void bubbleSort(int paths, int a[paths][VALUES]){
+void bubbleSort(int edgeAmount, edge_s edgeArray[edgeAmount]){
     //Temporary values are intialized
-    int tempSource, tempDestination, tempWeight;
+    mine_s tempSource, tempDestination;
+    double tempDistance;
 
-    //We run through the 2D arrays weight value paths-1 times since the last postition is assumed correct if the rest is
-    for (int i = 0; i < (paths - 1); ++i){
-        //This is run through paths-1-i times since the comparison function is unneeded on already assumed correct values(i)
-        for (int j = 0; j < paths - 1 - i; ++j ){
+    //We run through the 2D arrays weight value edgeAmount-1 times since the last postition is assumed correct if the rest is
+    for (int i = 0; i < (edgeAmount - 1); ++i){
+        //This is run through EdgeAmount-1-i times since the comparison function is unneeded on already assumed correct values(i)
+        for (int j = 0; j < edgeAmount - 1 - i; ++j ){
             //A single weight value (j) in the array is compared to the one following it - If larger we swap their places
-            if (a[j][2] > a[j+1][2]){
-                tempSource = a[j+1][0];
-                tempDestination = a[j+1][1];
-                tempWeight = a[j+1][2];
+            if (edgeArray[j].distanceBetween > edgeArray[j+1].distanceBetween){
+                tempSource = edgeArray[j+1].sourceBomb;
+                tempDestination = edgeArray[j+1].destinationBomb;
+                tempDistance = edgeArray[j+1].distanceBetween;
 
 
-                a[j+1][0] = a[j][0];
-                a[j+1][1] = a[j][1];
-                a[j+1][2] = a[j][2];
+                edgeArray[j+1].sourceBomb = edgeArray[j].sourceBomb;
+                edgeArray[j+1].destinationBomb = edgeArray[j].destinationBomb;
+                edgeArray[j+1].distanceBetween = edgeArray[j].distanceBetween;
 
 
-                a[j][0] = tempSource;
-                a[j][1] = tempDestination;
-                a[j][2] = tempWeight;
+                edgeArray[j].sourceBomb = tempSource;
+                edgeArray[j].destinationBomb = tempDestination;
+                edgeArray[j].distanceBetween = tempDistance;
             }
         }
     }
 }
 
 //Runs the kruskal algorithm to make a minimum spanning tree from a distance-sorted index of travelpoints
-int **kruskalAlgo(int paths, int sortedArray[paths][VALUES]) {
-    int parent[paths];
-    int rank[paths];
-
-    int **MST = make2DArray(paths-1, 2);
+void kruskalAlgo(int edgeAmount, int bombAmount, double minCost, edge_s sortedArray[edgeAmount], edge_s MST[bombAmount-1]) {
+    int parent[edgeAmount];
+    int rank[edgeAmount];
     int j = 0;
 
     //Function to initialize parent[] and rank[]
-    makeSet(parent, rank, paths);
-
-    //To store the minimum cost
-    int minCost = 0;
+    makeSet(parent, rank, edgeAmount);
 
     printf("Following are the edges in the constructed MST\n");
-    for (int i = 0; i < paths; ++i) {
-        int v1 = findParent(parent, sortedArray[i][0]);
-        int v2 = findParent(parent, sortedArray[i][1]);
-        int wt = sortedArray[i][2];
+    for (int i = 0; i < edgeAmount; ++i) {
+        int v1 = findParent(parent, sortedArray[i].sourceBomb.mineNumber);
+        int v2 = findParent(parent, sortedArray[i].destinationBomb.mineNumber);
 
         //If the parents are different that means they are in different sets so we union them
         if (v1 != v2) {
             unionSet(v1, v2, parent, rank);
-            minCost += wt;
-            MST[j][0] = sortedArray[i][0];
-            MST[j][1] = sortedArray[i][1];
+            minCost += sortedArray[i].distanceBetween;
+            MST[j].sourceBomb = sortedArray[i].sourceBomb;
+            MST[j].destinationBomb = sortedArray[i].destinationBomb;
+            MST[j].distanceBetween = sortedArray[i].distanceBetween;
             ++j;
-            printf("%d -- %d == %d\n", sortedArray[i][0], sortedArray[i][1], wt);
+            printf("%d -- %d == %lf\n", MST[j].sourceBomb.mineNumber, MST[j].destinationBomb.mineNumber, MST[j].distanceBetween);
         }
     }
 
-    printf("Minimum Cost Spanning Tree: %d\n", minCost);
-    if(MST == NULL) {
-        return EXIT_FAILURE;
-    } else {
-        return MST;
-    }
+    printf("Minimum Cost Spanning Tree: %lf\n", minCost);
 }
 
 //Initialization of arrays for later use
@@ -138,18 +127,12 @@ void unionSet(int u, int v, int parent[], int rank[]) {
 
 
 //Finds the minimum spanning tree to later be used in christofides to make an efficient route
-double **makeMST(int paths, int travelArray[paths][VALUES]) {
+void makeMST(int edgeAmount, int bombAmount, double minCost, edge_s edgeArray[edgeAmount], edge_s MST[bombAmount-1]) {
     //First we sort the array of possible travelpoints so that we can access the minimum distance-costs
-    bubbleSort(paths, travelArray);
+    bubbleSort(edgeAmount, edgeArray);
 
     //We then apply our sorted array to our kruskal algorithm to get a MST
-    int **MST = kruskalAlgo(paths, travelArray);
-
-    if (MST == NULL) {
-        exit(EXIT_FAILURE);
-    } else {
-        return MST;
-    }
+    kruskalAlgo(edgeAmount, bombAmount, minCost, edgeArray, MST);
 }
 
 int *makeSubgraph(int **MST) {
@@ -164,24 +147,6 @@ int *makeSubgraph(int **MST) {
 
 
 
-//Mallocs space for a 2D array to be used later
-double **make2DArray(int rows, int columns) {
-    double **result = malloc(rows * sizeof(double*));
-
-    for(int i = 0; i < rows; i++) {
-        result[i] = malloc(columns*sizeof(double));
-    }
-
-    return result;
-}
-
-void freeArray(double **array, double rows) {
-    for (int i = 0; i < rows; ++i) {
-        free(array[i]);
-    }
-    free(array);
-}
-
 
 
 
@@ -195,11 +160,15 @@ void christofides(mine_s bombs[], int bombAmount) {
     int edgeAmount = findEdgeAmount(bombAmount);
 
     //Build the complete graph of distances between bombs
-    double **edgeArray = structTo2D(bombs, bombAmount, edgeAmount);
+    edge_s edgeArray[bombAmount];
+    structToStruct(bombs, bombAmount, edgeAmount, edgeArray);
 
     //Step 1: Find Minimum Spanning Tree
-    int parent[MAX];
-    //primMST(graph, bombAmount, parent);
+    edge_s MST[bombAmount-1];
+    double minCost = 0;
+    makeMST(edgeAmount, bombAmount, minCost, edgeArray, MST);
+
+
 
     //Step 2: Find nodes with odd degree
     int oddVertices[MAX], oddCount = 0;
--- src/LowBoundTSP.h
+++ src/LowBoundTSP.h
@@ -4,16 +4,23 @@ typedef struct
     int x; // x-coordinate of mine
     int y; // y-coordinate of mine
     double tw; // Terrain-weight (coefficient) at mine location
-    int minenumber;
+    int mineNumber;
+    int edgeAmount;
 } mine_s;
+
+typedef struct {
+    mine_s sourceBomb;
+    mine_s destinationBomb;
+    double distanceBetween;;
+} edge_s;
 
 int findEdgeAmount(int bombAmount);
 double **make2DArray(int rows, int columns);
-double **structTo2D(mine_s bombs[], int bombAmount, int edgeAmount);
+void structToStruct(mine_s bombs[], int bombAmount, int , edge_s edgeArray[]);
 double calculateDistance(mine_s bomb1, mine_s bomb2);
-void bubbleSort(int paths, int a[paths][VALUES]);
+void bubbleSort(int edgeAmount, edge_s edgeArray[edgeAmount]);
 
-int **kruskalAlgo(int paths, int sortedArray[paths][VALUES]);
+void kruskalAlgo(int edgeAmount, int bombAmount, double minCost, edge_s edgeArray[edgeAmount], edge_s MST[bombAmount-1]);
     void makeSet(int parent[], int rank[], int n);
     int findParent(int parent[], int component);
     void unionSet(int u, int v, int parent[], int rank[]);
@@ -22,5 +29,5 @@ int **kruskalAlgo(int paths, int sortedArray[paths][VALUES]);
 
 
 
-void christofides(mine_s bombs[], int bombamount);
-    double **makeMST(int paths, int travelArray[paths][VALUES]);
+void christofides(mine_s bombs[], int bombAmount);
+    void makeMST(int edgeAmount, int bombAmount, double minCost, edge_s edgeArray[edgeAmount], edge_s MST[bombAmount-1]);
